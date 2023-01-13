cd97969e2fa8dbb2cfbba7bb0fc515a68b925e6c
Danil <danil050902@yandex.ru>
2023-01-13 14:36:26 +0300

    add header

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header =  ()=> {
     return (
-        <header className="header">Header</header>
+        <header className="header">Header header</header>
     )
 }
 
